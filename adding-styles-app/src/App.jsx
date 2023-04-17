c78001604ee9be29c20eb79dcdcce9aad0597f44
Edmon Sebit <edmontheprogrammer@gmail.com>
2023-04-17 09:23:18 +0200

    adding-styles-app initial commit

---
+++ adding-styles-app/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+/*
+  Adding Styles: 
+
+  In React, you specify a CSS class with "className". It works the same way as the HTML "class" attribute:
+
+      <img className="avatar" />
+
+Then you write the CSS rules for it in a separate CSS file. 
+    
+    // In your CSS file
+    .avatar {
+      border-radius: 50%; 
+    }
+
+  React does not prescribe how you add CSS files. In the simplest case. you'll add a "<link>" tag to your HTML. 
+  If you use a build too or a framework, consult its documentation to learn how to add a CSS file to your project. 
+*/
+
+function App() {
+
+  return (
+    <div className="App">
+      
+    </div>
+  )
+}
+
+export default App
