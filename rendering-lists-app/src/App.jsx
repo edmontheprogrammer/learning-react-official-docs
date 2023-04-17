2f65228f6b2399d4aaded8486d0164f3912f2376
Edmon Sebit <edmontheprogrammer@gmail.com>
2023-04-17 10:21:47 +0200

    rendering-list-app initial commit

--- rendering-lists-app/src/App.jsx
+++ rendering-lists-app/src/App.jsx
@@ -3,32 +3,70 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+/*
+  Rendering lists
+
+  You will rely on JavaScript features like "for loop" and the "array map() function" to render lists of components
+
+  For example, let's say you have an array of products
+
+  const products = [
+    {title: 'Cabbage', id:1}, 
+    {title: 'Garlic', id: 2}, 
+    {title: 'Apple', id: 3},
+  ];
+
+  Inside your component, use the "map()" function to transform an array of products into an array of "<li>" items:
+
+
+  const listItems = products.map(product => 
+      <li key={product.id}>
+          {product.title}
+      </li>
+    ); 
+
+    return (
+      <ul>{listItems}</ul>
+    );
+
+    Notice how "<li>" has a "key" attribute. For each item in a list, you should pass a string or a number uniquely identifies 
+    that item among its sibilings. usually, a key should coming from your data, such as a database ID. React uses your keys to know
+    what happened if you later insert, delete, or reorder the items
+
+*/
+
+
+function App() {
+
+  // creating a list of objects. Each object has three attributes or key-value pairs: title,
+  // isFruit and id that are assigned values
+ const products = [
+  {title: 'Cabbage', isFruit: false, id: 1}, 
+  {title: 'Garlic', isFruit: false, id: 2}, 
+  {title: 'Apple', isFruit: true, id: 3}, 
+];
+
+  /*
+    Creating "listItems" and assigning it to a function call,map(), that runs on the objects array above. 
+    it takes each objects in the products array and converts it into an HTMl list item with the given values. 
+    <ul>{listItems}</ul> shows each list item on the web page. 
+  */
+  const listItems = products.map(product => 
+      <li
+        key={product.id}
+        style={{
+          color: product.isFruit ? 'magenta' : 'darkgreen'
+        }}
+      >
+      {product.title}
+
+      </li>
+    ); 
+  
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    
+      <ul>{listItems}</ul>
   )
 }
 
